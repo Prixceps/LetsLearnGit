7895ccf1e413ad31d2d15b41d5e7058c466fb8e5
prixceps <prrj2026@gmail.com>
2023-08-02 01:21:22 +0530

    ARRAY MADE

--- add.cpp
+++ add.cpp
@@ -1,4 +1,18 @@
 #include<iostream>
-void main(){
-    printf("hello world");
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter size of array";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter your number at"<<i+1<<"th position";
+        cin>>arr[i];
+    }
+    cout<<"reversed array is";
+    for(int i=n-1;i>=0;i--){
+        cout<<arr[i];
+    }
+    return 0;
+    
 }
